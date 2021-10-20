3e34253d26cddfef68a52c496f101e01142fa932
Kenneth Newman <kennethnewman80@gmail.com>
2021-10-19 20:29:08 -0500

    create my player module

---
+++ player.js
@@ -0,0 +1,16 @@
+
+
+
+
+class Player {
+    constructor(name) {
+    this.name = name;
+    this.listOfGestures = ["rock","paper","scissors","lizard","spock"];
+    this.gestureSelection;
+    this.score = 0;
+   
+    }
+
+}
+
+module.exports = Player;
