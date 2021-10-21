b65c6d6a026a3d29e039baf59fb277b2ab299f7c
Kenneth Newman <kennethnewman80@gmail.com>
2021-10-21 17:19:29 -0500

    update player info in game module

--- game.js
+++ game.js
@@ -7,8 +7,46 @@ class Game {
         this.playerOne = new Human(" ");
         this.playerTwo = new Human(" ");
 
-        this.gestures = [];
+        this.listOfGestures = [];
 
     }
+
+    playerInfo() {
+        let playerOneinfo = prompt("what is player ones's name?")
+        this.playerOne = new Human(playerOneinfo);
+        let playerTwoinfo = prompt("Would you like to play with another human or ai")
+        if (playerTwoinfo === "human") {
+            let playerTwoinfo = prompt("what is player two's name?")
+            this.playerTwo = new Human(playerTwoinfo);
+        }
+        else {
+            this.playerTwo = new Ai
+            console.log("You have chosen to play against an AI.");
+        }
+
+    }
+
+    comparingGestures() {
+
+        let listOfGestures
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
