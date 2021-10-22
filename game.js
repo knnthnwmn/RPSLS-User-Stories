7cc4c858a2d17c7bae40466333399f2cefcd8f0e
Kenneth Newman <kennethnewman80@gmail.com>
2021-10-21 19:38:09 -0500

    completed (run) function and display function

--- game.js
+++ game.js
@@ -28,25 +28,124 @@ class Game {
 
     comparingGestures() {
 
-        let listOfGestures
+        let listOfGestures = ["rock","paper","scissors","lizard","spock"];
+        if (this.playerOne.gestureSelection === this.playerTwo.gestureSelection) {
+            console.log("its a tie")
+        }
+        else {
+            switch (this.playerOne.gestureSelection) {
+                case "rock":
+                    if (this.playerTwo.gestureSelection === "scissors") {
+                        this.playerOne.sore++
+                        console.log("Rock crushes scissors!");
+                    }
+                    else if (this.playerTwo.gestureSelection === "lizard") {
+                        this.playerOne.sore++
+                        console.log("Rock splats lizard!");
+                    }
+                    else {
+                        this.playerTwo++
+                        console.log("Player two wins!");
+                    }
+                        break;
+
+                case "paper":
+                    if (this.playerTwo.gestureSelection === "rock") {
+                            this.playerOne.sore++
+                            console.log("Paper covers rock!");
+                    }
+                    else if (this.playerTwo.gestureSelection === "spock") {
+                            this.playerOne.sore++
+                            console.log("Spock disproved by paper!");
+                    }
+                    else {
+                            this.playerTwo++
+                            console.log("Player two wins!")
+                    }
+                        break;
+
+                case "scissors":
+                    if (this.playerTwo.gestureSelection === "paper") {
+                                this.playerOne.sore++
+                                console.log("Scissors cut paper!");
+                    }
+                    else if (this.playerTwo.gestureSelection === "lizard") {
+                                this.playerOne.sore++
+                                console.log("Lizard decapitated by scissors!");
+                    }
+                    else {
+                                this.playerTwo++
+                                console.log("Player two wins!")
+                    }
+                        break; 
+                case "spock":
+                            if (this.playerTwo.gestureSelection === "rock") {
+                                    this.playerOne.sore++
+                                    console.log("Spock vaporizes rock!");
+                    }
+                            else if (this.playerTwo.gestureSelection === "scissors") {
+                                    this.playerOne.sore++
+                                    console.log("Spock smashes scissors!");      
+                    }
+                            else {
+                                    this.playerTwo++
+                                    console.log("Player two wins!")
+                    }  
+                        break;
+                        case "lizard":
+                            if (this.playerTwo.gestureSelection === "spock") {
+                                    this.playerOne.sore++
+                                    console.log("Lizard poisons Spock!");
+                    }
+                            else if (this.playerTwo.gestureSelection === "paper") {
+                                    this.playerOne.sore++
+                                    console.log("Lizard eats paper!");       
+                    }
+                            else {
+                                    this.playerTwo++
+                                    console.log("Player two wins!")
+                    }  
+                        break;        
+                default:
+                        break;
+            
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
+
+
+    runGame() {
+        this.playerInfo();
+        this.displayMessage();
+        //Call a function that asks how many players and then creates appropriate human/AI for playerOne, playerTwo
+
+        while (this.playerOne.score < 3 && this.playerTwo.score < 3) {
+            this.playerOne.chooseGesture(); 
+            this.playerTwo.chooseGesture();
+            this.compareGestures();
+        }
+        this.displayGameWinner();
+    }
+
+    welcomeMessage() {
+        console.log("Welcome to Rock, Paper, Scissors, Lizard, Spock.");
+        console.log("You will have the choice to play with an AI or a second player. Please make a choice.");
+        console.log("Alright. To win the game, Player one or Player two will need 3 points to win.");
+        console.log("The way this works is, rock crushes scissors, scissors cuts paper, paper covers rock, rock crushes lizard, lizard poisons Spock, Spock smashes scissors, scissors decapitates lizard, lizard eats paper, paper disproves Spock, and Spock vaporizes rock.");
+        console.log("If you understand how this works, let the game begin!");
+    }
+
+    displayGameWinner() {
+        if (this.playerOne > this.playerTwo) {
+        console.log(this.playerOne.name + " wins this game!");
+        }
+        else if (this.playerOne < this.playerTwo) {
+            console.log(this.playerTwo.name + " wins this game!");
+            }
+    }
+}
+
+
+
+module.exports = Game;
